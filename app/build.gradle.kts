a5e3bece653e915e7398f1ec9838228613133922
germansubbotin99-art <germansubbotin99@gmail.com>
2026-08-24 20:46:42 +0300

    Bump custom Landscape+ build to v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "tech.devline.scropy_ui"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1-landscape-plus"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
