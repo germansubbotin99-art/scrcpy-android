a249a13615c697723b9788a7c66673be7c76e161
germansubbotin99-art <germansubbotin99@gmail.com>
2026-08-24 22:00:58 +0300

    Add STITCHLINK Orbita neon palette

--- app/src/main/java/tech/devline/scropy_ui/ui/theme/Color.kt
+++ app/src/main/java/tech/devline/scropy_ui/ui/theme/Color.kt
@@ -2,10 +2,23 @@ package tech.devline.scropy_ui.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// STITCHLINK • ОРБИТА palette
+val OrbitaBlue = Color(0xFF35C8FF)
+val OrbitaBlueSoft = Color(0xFF8BE4FF)
+val OrbitaViolet = Color(0xFF8B5CFF)
+val OrbitaMagenta = Color(0xFFE34DFF)
+val OrbitaNavy = Color(0xFF070B22)
+val OrbitaNavy2 = Color(0xFF0D1230)
+val OrbitaSurface = Color(0xFF12183A)
+val OrbitaSurface2 = Color(0xFF1A2149)
+val OrbitaText = Color(0xFFF1F5FF)
+val OrbitaTextMuted = Color(0xFFAAB6DC)
+val OrbitaError = Color(0xFFFF6685)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Kept for compatibility with older code
+val Purple80 = OrbitaBlueSoft
+val PurpleGrey80 = OrbitaViolet
+val Pink80 = OrbitaMagenta
+val Purple40 = OrbitaBlue
+val PurpleGrey40 = OrbitaViolet
+val Pink40 = OrbitaMagenta
