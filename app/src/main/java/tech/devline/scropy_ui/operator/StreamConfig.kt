aa17d10badcb619e77d3798637406099fb1943fa
germansubbotin99-art <germansubbotin99@gmail.com>
2026-08-25 12:19:05 +0300

    STITCHLINK ORBITA: add stream profile controller

---
+++ app/src/main/java/tech/devline/scropy_ui/operator/StreamConfig.kt
@@ -0,0 +1,37 @@
+package tech.devline.scropy_ui.operator
+
+object StreamConfig {
+
+    enum class Mode {
+        ECONOMY,
+        FLIGHT,
+        MAXIMUM
+    }
+
+    data class Profile(
+        val fps: Int,
+        val bitrate: Int
+    )
+
+    @Volatile
+    var currentMode: Mode = Mode.FLIGHT
+
+    fun current(): Profile {
+        return when (currentMode) {
+            Mode.ECONOMY -> Profile(
+                fps = 30,
+                bitrate = 4_000_000
+            )
+
+            Mode.FLIGHT -> Profile(
+                fps = 60,
+                bitrate = 12_000_000
+            )
+
+            Mode.MAXIMUM -> Profile(
+                fps = 60,
+                bitrate = 20_000_000
+            )
+        }
+    }
+}
